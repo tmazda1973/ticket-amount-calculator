d8bfb96959a7aedac52db064ccf14db1fca5a936
t.matsuda <tmazda1973@gmail.com>
2023-10-30 13:19:30 +0900

    [modify] 実装見直し

--- app/commands/ticket_amount/input_receipt_operator.rb
+++ app/commands/ticket_amount/input_receipt_operator.rb
@@ -1,3 +1,5 @@
+require_relative '../../enums/ticket_amount/special_condition'
+
 module TicketAmount
   #
   # CLIからの入力を制御するクラスです。
@@ -65,7 +67,7 @@ module TicketAmount
     # @private
     #
     def _input_senior_ticket_count(cli)
-      prompt = 'チケット枚数(シニア) [数字]: '
+      prompt = 'チケット枚数(シニア) [数字]:'
       error_message = '入力値に誤りがあります。再入力してください。(コマンド終了: Q|q)'
       input = cli.ask(prompt)
       until input =~ /^[1-9]\d*$/
@@ -83,8 +85,37 @@ module TicketAmount
     # @private
     #
     def _input_special_conditions(cli)
-      value = []
-      value
+      values = [] # 入力値
+      prompt = <<-PROMPT
+特別条件を入力してください。カンマ区切りで複数入力できます。(例: 1,2,3)
+  1: 夕方料金
+  2: 休日料金
+  3: 月水割引
+
+特別条件 [数字(複数入力可)]:
+      PROMPT
+      error_message = '入力値に誤りがあります。再入力してください。(コマンド終了: Q|q)'
+      input = cli.ask(prompt)
+      until self._valid_special_conditions(input)
+        cli.say(error_message)
+        input = cli.ask(prompt)
+        cli.exit(0) if input.downcase == 'q'
+      end
+      values
+    end
+
+    #
+    # 特別条件の入力値が正しいかを判定します。
+    # @param input [String] 入力値
+    # @return [Boolean] true 正しい、false 正しくない
+    # @private
+    #
+    def _valid_special_conditions(input)
+      allow_values = SpecialCondition.all # 許可する入力値
+      input.split(',').each do |value|
+        return false unless allow_values.include?(value.to_i)
+      end
+      true
     end
   end
 end
