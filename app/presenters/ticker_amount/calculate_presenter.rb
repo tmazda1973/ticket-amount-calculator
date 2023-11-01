00f9a7c5dcfbb4ab37a0f093502b27a6162e4ccd
t.matsuda <tmazda1973@gmail.com>
2023-11-01 10:12:30 +0900

    [modify] 実装見直し

--- app/presenters/ticker_amount/calculate_presenter.rb
+++ app/presenters/ticker_amount/calculate_presenter.rb
@@ -32,21 +32,41 @@ module TicketAmount
       price_hash = self._build_output_price(self.ticket_type) # 単価
       count_hash = self._build_output_count(self.adult_ticket_count, self.child_ticket_count, self.senior_ticket_count) # 枚数
       amount_hash = self._build_output_amount(self.ticket_type, self.adult_ticket_count, self.child_ticket_count, self.senior_ticket_count) # 合計金額
+      total_amount_hash = self._build_output_total_amount(self.before_change_total_amount, self.total_amount) # 合計金額
       # 計算結果を出力する
       @cli.echo_hr
       @cli.say("大人  ：#{price_hash[:adult]} 円 x #{count_hash[:adult]} 枚 = #{amount_hash[:adult]} 円")
       @cli.say("子供  ：#{price_hash[:child]} 円 x #{count_hash[:child]} 枚 = #{amount_hash[:child]} 円")
       @cli.say("シニア：#{price_hash[:senior]} 円 x #{count_hash[:senior]} 枚 = #{amount_hash[:senior]} 円")
       @cli.say("")
-      @cli.say("金額変更前合計金額：#{self.number_format(self.before_change_total_amount)} 円")
+      @cli.say("金額変更前合計金額：")
+      @cli.say(" #{total_amount_hash[:before]} 円")
+      @cli.say("販売合計金額：")
+      @cli.say(" #{total_amount_hash[:after]} 円")
+      @cli.say("")
       @cli.say("金額変更明細：")
       @cli.say("#{self.change_amount_detail}") if (!self.change_amount_detail.nil? && !self.change_amount_detail.empty?)
-      @cli.say("")
-      @cli.say("販売合計金額：#{self.number_format(self.total_amount)} 円")
       @cli.echo_hr
     end
 
     private
+    #
+    # 出力用の合計金額情報を構築します。
+    # @param before_amount [Integer,BigDecimal] 金額変更前合計金額
+    # @param after_amount [Integer,BigDecimal] 販売合計金額
+    # @return [Hash<Symbol>] 出力用の合計金額情報
+    # @private
+    #
+    def _build_output_total_amount(before_amount , after_amount)
+      before = self.number_format(before_amount)
+      after = self.number_format(after_amount)
+      max_len = [before.length, after.length].max
+      {
+        before: before.rjust(max_len),
+        after: after.rjust(max_len)
+      }
+    end
+
     #
     # 出力用の単価情報を構築します。
     # @param ticket_type [Integer] チケット種別
@@ -57,7 +77,7 @@ module TicketAmount
       adult = self.number_format(self.adult_ticket_price(ticket_type))
       child = self.number_format(self.child_ticket_price(ticket_type))
       senior = self.number_format(self.senior_ticket_price(ticket_type))
-      max_len = self._max_str_len(adult, child, senior)
+      max_len = [adult.length, child.length, senior.length].max
       {
         adult: adult.rjust(max_len),
         child: child.rjust(max_len),
@@ -77,7 +97,7 @@ module TicketAmount
       adult = self.number_format(adult_count)
       child = self.number_format(child_count)
       senior = self.number_format(senior_count)
-      max_len = self._max_str_len(adult, child, senior)
+      max_len = [adult.length, child.length, senior.length].max
       {
         adult: adult.rjust(max_len),
         child: child.rjust(max_len),
@@ -98,24 +118,12 @@ module TicketAmount
       adult = self.number_format(self.calc_adult_ticket_amount(ticket_type, adult_count))
       child = self.number_format(self.calc_child_ticket_amount(ticket_type, child_count))
       senior = self.number_format(self.calc_senior_ticket_amount(ticket_type, senior_count))
-      max_len = self._max_str_len(adult, child, senior)
+      max_len = [adult.length, child.length, senior.length].max
       {
         adult: adult.rjust(max_len),
         child: child.rjust(max_len),
         senior: senior.rjust(max_len)
       }
-    end
-
-    #
-    # 文字列の最大桁数を取得します。
-    # @param adult [String] 大人の文字列
-    # @param child [String] 子供の文字列
-    # @param senior [String] シニアの文字列
-    # @return [Integer] 最大桁数
-    # @private
-    #
-    def _max_str_len(adult, child, senior)
-      [adult.length, child.length, senior.length].max
     end
   end
 end
